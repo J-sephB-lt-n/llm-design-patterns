d2bcc47181992a7f55ed236881ca30175a105732
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2025-07-05 22:03:22 +0200

    refactor(main.py-pyproject.toml-uv.lock): changed flow of app setup (model api first)

--- memory/chatbot_memory_visualiser/main.py
+++ memory/chatbot_memory_visualiser/main.py
@@ -7,19 +7,18 @@ from typing import Final
 
 import httpx
 import streamlit as st
+from loguru import logger
 
 
 def landing_page():
     st.title("Landing Page")
 
 
-def setup_page():
-    st.title("Setup")
-
-    def fetch_llm_names(base_url: str, api_key: str) -> list[str] | None:
-        """
-        Returns a list of model names, assuming that `base_url` is an OpenAI compatible API
-        """
+def fetch_llm_names(base_url: str, api_key: str) -> list[str] | None:
+    """
+    Returns a list of model names from an OpenAI-compatible API
+    """
+    try:
         resp = httpx.get(
             url=f"{base_url}/v1/models",
             headers={
@@ -31,41 +30,79 @@ def setup_page():
             return None
 
         return sorted([x["id"] for x in resp.json()["data"]])
+    except Exception as error:
+        logger.exception("Unexpected error")
+        return None
 
-    if "llm_settings_saved" not in st.session_state:
-        st.session_state.llm_settings_saved = False
-        st.session_state.llm_name = ""
-        st.session_state.llm_temperature = 1.0
+
+def setup_page():
+    st.title("Setup")
+    if "llm_api_is_valid" not in st.session_state:
+        st.session_state.llm_api_is_valid = False
         st.session_state.llm_api_base_url = ""
         st.session_state.llm_api_key = ""
-    with st.form(key="model_setup_form"):
-        llm_temperature = st.number_input(
-            "Model Temperature",
-            value=st.session_state.llm_temperature,
-            step=0.01,
-        )
+        st.session_state.available_llm_names = []
+
+    if "llm_params_saved" not in st.session_state:
+        st.session_state.llm_params_saved = False
+        st.session_state.llm_name = ""
+        st.session_state.llm_temperature = 1.0
+
+    with st.form(key="llm_api_setup_form"):
         llm_api_base_url = st.text_input(
             "Model API Base URL", value=st.session_state.llm_api_base_url
         )
         llm_api_key = st.text_input(
             "Model API Key", value=st.session_state.llm_api_key, type="password"
         )
+        llm_api_submit_button = st.form_submit_button(label="Test Model API")
+        if llm_api_submit_button:
+            if not llm_api_base_url or not llm_api_key:
+                st.error("Please provide Model API url and API key")
+                return
 
-        llm_setup_submit_button = st.form_submit_button(label="Submit")
-        if llm_setup_submit_button:
-            if len(llm_name) == 0:
-                st.error("Model name cannot be blank")
-            elif not (0.0 <= llm_temperature <= 2.0):
-                st.error("Temperature must be in the range [0, 2]")
-            elif len(llm_api_base_url) == 0:
-                st.error("Model API Base URL cannot be blank")
-            elif len(llm_api_key) == 0:
-                st.error("Model API Key cannot be blank")
-            else:
-                st.session_state.llm_name = llm_name
-                st.session_state.llm_temperature = llm_temperature
-                st.session_state.llm_settings_saved = True
-                st.success("Model settings saved successfully")
+            llm_names: list[str] | None = fetch_llm_names(
+                base_url=llm_api_base_url,
+                api_key=llm_api_key,
+            )
+            if llm_names is None:
+                st.error("Provided Model API is Invalid")
+                return
+            st.session_state.llm_api_is_valid = True
+            st.session_state.available_llm_names = llm_names
+            st.success(f"Model API is Valid (found {len(llm_names)} models)")
+
+        # with st.form(key="model_setup_form"):
+        #     llm_temperature = st.number_input(
+        #         "Model Temperature",
+        #         value=st.session_state.llm_temperature,
+        #         step=0.01,
+        #     )
+        # if llm_api_base_url and llm_api_key:
+        #     models_list: list[str] | None = fetch_llm_names(
+        #         base_url=llm_api_base_url,
+        #         api_key=llm_api_key,
+        #     )
+        #     if models_list is None:
+        #         st.error("Model API is invalid")
+        #     else:
+        #         st.json(models_list)
+        #
+        # llm_setup_submit_button = st.form_submit_button(label="Submit")
+        # if llm_setup_submit_button:
+        #     # if len(llm_name) == 0:
+        #     #     st.error("Model name cannot be blank")
+        #     if not (0.0 <= llm_temperature <= 2.0):
+        #         st.error("Temperature must be in the range [0, 2]")
+        #     elif len(llm_api_base_url) == 0:
+        #         st.error("Model API Base URL cannot be blank")
+        #     elif len(llm_api_key) == 0:
+        #         st.error("Model API Key cannot be blank")
+        #     else:
+        #         # st.session_state.llm_name = llm_name
+        #         st.session_state.llm_temperature = llm_temperature
+        #         st.session_state.llm_settings_saved = True
+        #         st.success("Model settings saved successfully")
 
 
 def chat_page():
