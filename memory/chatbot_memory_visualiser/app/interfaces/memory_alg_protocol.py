3699bee8226569ec50bb4a68ec40edc147c33ccf
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2025-07-07 20:40:04 +0200

    feat(memory_alg_protocol.py-memory_algs/full_chat_history.py): interim commit

--- memory/chatbot_memory_visualiser/app/interfaces/memory_alg_protocol.py
+++ memory/chatbot_memory_visualiser/app/interfaces/memory_alg_protocol.py
@@ -1,9 +1,10 @@
 """
-TODO
+Required interface which all memory algorithms must follow to integrate with this app
 """
 
 from typing import Literal, Protocol
 
+import openai
 from pydantic import BaseModel
 
 
@@ -14,17 +15,19 @@ class ChatMessage(BaseModel):
 
 class ChatMessageDetail(BaseModel):
     """
-    All messages within a single turn of the conversation
+    All messages within a single turn of the conversation (a single chat completion)
 
     Attributes:
         visible_messages (list[ChatMessage]): Messages visible to the chatting end-user
         all_messages (list[ChatMessage]): All messages involved in generating the chat \
             messages served to the chatting end-user (system messages, tool calls, memory \
             management by the LLM etc.)
+        token_usage (dict): Token usage breakdown consumed by this single chat completion
     """
 
     visible_messages: list[ChatMessage]  # a subset of `all_messages`
     all_messages: list[ChatMessage]  # contains `visible_messages`
+    token_usage: dict
 
 
 class MemoryAlg(Protocol):
@@ -32,10 +35,11 @@ class MemoryAlg(Protocol):
     Required behaviour of a chat-based memory algorithm
     """
 
-    alg_name: str
     chat_history: list[ChatMessageDetail]
+    system_prompt: str | None
+    llm_client: openai.OpenAI
 
-    def chat(self, str) -> None:
+    def chat(self, user_msg: str) -> None:
         """
         Process a new user message, update memory and chat history
         """
---
+++ memory/chatbot_memory_visualiser/app/memory_algs/full_chat_history.py
@@ -0,0 +1,61 @@
+"""
+Memory algorithm which simply stores the entire chat history
+"""
+
+import openai
+
+from app.interfaces.memory_alg_protocol import ChatMessage, ChatMessageDetail, MemoryAlg
+
+
+class StoreFullChatHistory(MemoryAlg):
+    """
+    Memory algorithm which just stores the entire chat history
+    """
+
+    def __init__(
+        self,
+        system_prompt: str | None,
+        llm_client: openai.OpenAI,
+        llm_name: str,
+        llm_temperature: float,
+    ) -> None:
+        self.chat_history: list[ChatMessageDetail] = []
+        self.llm_client = llm_client
+        self.llm_name: str = llm_name
+        self.llm_temperature = llm_temperature
+        self.system_prompt: str | None = system_prompt
+
+        if self.system_prompt:
+            self.chat_history.append(
+                ChatMessageDetail(
+                    visible_messages=[],
+                    all_messages=[
+                        ChatMessage(
+                            role="system",
+                            content=system_prompt,
+                        )
+                    ],
+                    token_usage={},
+                )
+            )
+
+    def chat(self, user_msg: str) -> None:
+        """
+        Process a new user message, update memory and chat history
+        """
+        chat_msgs_to_llm: list[dict] = [
+            msg.model_dump() for msg in self.chat_history[-1].all_messages
+        ]
+        user_msg = ChatMessage(role="user", content=user_msg)
+        chat_msgs_to_llm.append(user_msg.model_dump())
+        llm_response = self.llm_client.chat.completions.create(
+            model=self.llm_name,
+            temperature=self.llm_temperature,
+            messages=chat_msgs_to_llm,
+        )
+        chat_msgs_to_llm.append(
+            {
+                "role": llm_response.choices[0].message.role,
+                "content": llm_response.choices[0].message.content,
+            }
+        )
