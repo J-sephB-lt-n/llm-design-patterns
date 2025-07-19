8cd0490c0cf719a90947e4580505c92f213aab45
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2025-07-19 22:05:05 +0200

    feat(memory_algs/vector_memory.py): added methods add_vector_memory() and fetch_relevant_memories()

--- memory/chatbot_memory_visualiser/app/memory_algs/vector_memory.py
+++ memory/chatbot_memory_visualiser/app/memory_algs/vector_memory.py
@@ -8,8 +8,10 @@ from typing import Literal
 
 import lancedb
 import model2vec
+import numpy as np
 import openai
 import pyarrow as pa
+from lancedb.rerankers import RRFReranker
 
 from app.interfaces.memory_alg_protocol import ChatMessage, ChatMessageDetail, MemoryAlg
 
@@ -30,8 +32,8 @@ class VectorMemory(MemoryAlg):
         n_vector_memories_to_fetch (int): In every chat completion, this number of chat messages will be 
                                             fetched from the full chat history (vector database) 
                                             and included in the model prompt.
-        vector_embed_method (str): Approach used for embedding messages in long-term vector storage
-                                    One of ['full_text_search', 'semantic_dense', 'hybrid']
+        vector_search_method (str): Search algorithm used for fetching memories from the vector database
+                                    One of ['semantic_dense', 'hybrid']
         vector_memory_type (str): Format used to generate vector memories.
                                     "chat_message" simply stores each chat message directly.
                                     "extracted_facts" uses an LLM to extract facts from each chat \
@@ -45,9 +47,7 @@ class VectorMemory(MemoryAlg):
         llm_temperature: float,
         n_chat_messages_in_working_memory: int = 10,
         n_vector_memories_to_fetch: int = 5,
-        vector_embed_method: Literal[
-            "full_text_search", "semantic_dense", "hybrid"
-        ] = "hybrid",
+        vector_search_method: Literal["semantic_dense", "hybrid"] = "hybrid",
         vector_memory_type: Literal["chat_message", "extracted_facts"] = "chat_message",
     ) -> None:
         self.chat_history: list[ChatMessageDetail] = []
@@ -56,14 +56,14 @@ class VectorMemory(MemoryAlg):
         self.llm_temperature = llm_temperature
         self.n_chat_messages_in_working_memory = n_chat_messages_in_working_memory
         self.n_vector_memories_to_fetch = n_vector_memories_to_fetch
-        self.vector_embed_method = vector_embed_method
+        self.vector_search_method = vector_search_method
         self.vector_memory_type = vector_memory_type
 
         self.embed_model = model2vec.StaticModel.from_pretrained(
             "minishlab/potion-retrieval-32M"
         )
         self.vector_db = lancedb.connect(Path("temp_files/vector_memory_db"))
-        self.vector_db.create_table(
+        self.vector_memories = self.vector_db.create_table(
             name="memories",
             schema=pa.schema(
                 [
@@ -78,6 +78,50 @@ class VectorMemory(MemoryAlg):
                 ],
             ),
         )
+        self.vector_memories.create_fts_index("text")
+        self.vector_memories.wait_for_index("text_idx")
+        self.reranker = RRFReranker()
+
+    def add_vector_memory(self, text: str) -> None:
+        """
+        Write a memory into the vector database
+        """
+        self.vector_memories.add(
+            [
+                {
+                    "text": text,
+                    "vector": self.embed_model.encode(text),
+                }
+            ]
+        )
+
+    def fetch_relevant_memories(
+        self,
+        query: str,
+        n_to_fetch: int,
+        search_method: Literal["semantic_dense", "hybrid"],
+    ) -> list[str]:
+        """
+        Fetch `n_to_fetch` closest memories to `query` from the vector database using `search_method`
+        """
+        embed_query: np.ndarray = self.embed_model.encode(query)
+        match search_method:
+            case "semantic_dense":
+                search_results = (
+                    self.vector_memories.search(embed_query).limit(n_to_fetch).to_list()
+                )
+            case "hybrid":
+                search_results = (
+                    self.vector_memories.vector(embed_query)
+                    .text(query)
+                    .rerank(self.reranker)
+                    .limit(n_to_fetch)
+                    .to_list()
+                )
+            case _:
+                raise ValueError(f"Unknown search_method='{search_method}'")
+
+        return [x["text"] for x in search_results]
 
     def chat(self, user_msg: str) -> None:
         """
