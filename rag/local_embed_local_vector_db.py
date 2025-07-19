830c80669f49f2f3be10c484678dad547590bafb
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2025-07-19 21:41:13 +0200

    feat(rag/local_embed_local_vector_db.py): added hybrid search

--- rag/local_embed_local_vector_db.py
+++ rag/local_embed_local_vector_db.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from typing import Final
 
 import lancedb
+from lancedb.rerankers import RRFReranker
 import model2vec
 import numpy as np
 
@@ -38,16 +39,35 @@ db.create_table(
 
 docs_table = db.open_table("docs_table")
 
+# create index for Full-Text-Search #
+docs_table.create_fts_index("text")
+docs_table.wait_for_index(["text_idx"])
+
 queries = [
     "entertainment",
     "technology",
     "jungle",
 ]
 
+reranker = RRFReranker()
+
 for query in queries:
     embed_query: np.ndarray = embedder.encode(query)
     search_results = docs_table.search(embed_query).limit(1).to_list()
-    print(f"""closest doc to query '{query}' is '{search_results[0]["text"]}'""")
+    print(
+        f"""closest doc to query '{query}' using semantic (vector) search is '{search_results[0]["text"]}'"""
+    )
+
+    hybrid_search_results = (
+        docs_table.search(query_type="hybrid")
+        .vector(embed_query)
+        .text(query)
+        .limit(1)
+        .to_list()
+    )
+    print(
+        f"""closest doc to query '{query}' using hybrid search is '{hybrid_search_results[0]["text"]}'"""
+    )
 
 # delete the database #
 shutil.rmtree(DB_PATH)
