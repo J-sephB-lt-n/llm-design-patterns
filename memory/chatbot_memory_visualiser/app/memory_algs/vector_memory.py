251b6e20a3727f9686c02112bc1598e43f925b52
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2025-07-19 21:21:37 +0200

    feat(memory_alg/vector_memory.py): added model2vec and lancedb setup

--- memory/chatbot_memory_visualiser/app/memory_algs/vector_memory.py
+++ memory/chatbot_memory_visualiser/app/memory_algs/vector_memory.py
@@ -3,17 +3,21 @@ Memory algorithm which stores the few most recent chat messages and the full cha
 in a vector database
 """
 
+from pathlib import Path
 from typing import Literal
 
+import lancedb
+import model2vec
 import openai
+import pyarrow as pa
 
 from app.interfaces.memory_alg_protocol import ChatMessage, ChatMessageDetail, MemoryAlg
 
 
 class VectorMemory(MemoryAlg):
     """
-    Memory algorithm which stores the few most recent chat messages and the full chat history \
-in a vector database
+    Memory algorithm which stores only the few most recent chat messages, but also the full \
+    chat history in a vector database
 
     Attributes:
         llm_client (openai.OpenAI): Model API client
@@ -28,6 +32,10 @@ in a vector database
                                             and included in the model prompt.
         vector_embed_method (str): Approach used for embedding messages in long-term vector storage
                                     One of ['full_text_search', 'semantic_dense', 'hybrid']
+        vector_memory_type (str): Format used to generate vector memories.
+                                    "chat_message" simply stores each chat message directly.
+                                    "extracted_facts" uses an LLM to extract facts from each chat \
+                                        message then embeds these facts and writes them to the db. 
     """
 
     def __init__(
@@ -40,10 +48,39 @@ in a vector database
         vector_embed_method: Literal[
             "full_text_search", "semantic_dense", "hybrid"
         ] = "hybrid",
+        vector_memory_type: Literal["chat_message", "extracted_facts"] = "chat_message",
     ) -> None:
+        self.chat_history: list[ChatMessageDetail] = []
         self.llm_client = llm_client
         self.llm_name = llm_name
         self.llm_temperature = llm_temperature
         self.n_chat_messages_in_working_memory = n_chat_messages_in_working_memory
         self.n_vector_memories_to_fetch = n_vector_memories_to_fetch
         self.vector_embed_method = vector_embed_method
+        self.vector_memory_type = vector_memory_type
+
+        self.embed_model = model2vec.StaticModel.from_pretrained(
+            "minishlab/potion-retrieval-32M"
+        )
+        self.vector_db = lancedb.connect(Path("temp_files/vector_memory_db"))
+        self.vector_db.create_table(
+            name="memories",
+            schema=pa.schema(
+                [
+                    pa.field("text", pa.string()),
+                    pa.field(
+                        "vector",
+                        pa.list_(
+                            pa.float32(),
+                            512,
+                        ),
+                    ),
+                ],
+            ),
+        )
+
+    def chat(self, user_msg: str) -> None:
+        """
+        Process a new user message, update memory and chat history
+        """
+        ...
