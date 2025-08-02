633dbd9fae0675ed1101fc17831a2cb2a8e46234
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2025-08-02 22:53:41 +0200

    feat(memory_algs/__init__.py): added KnowledgeGraphMemory alg

--- memory/chatbot_memory_visualiser/app/memory_algs/__init__.py
+++ memory/chatbot_memory_visualiser/app/memory_algs/__init__.py
@@ -1,10 +1,17 @@
+"""
+List of all implemented memory algorithms
+"""
+
 from app.interfaces.memory_alg_protocol import MemoryAlg
+
+from app.memory_algs.knowledge_graph import KnowledgeGraphMemory
 from app.memory_algs.no_memory import NoMemory
 from app.memory_algs.recursive_summarisation import RecursiveSummarisation
 from app.memory_algs.store_full_chat_history import StoreFullChatHistory
 from app.memory_algs.vector_memory import VectorMemory
 
 memory_algs: dict[str, type[MemoryAlg]] = {
+    "Knowledge Graph": KnowledgeGraphMemory,
     "No Memory": NoMemory,
     "Recursive Summarisation": RecursiveSummarisation,
     "Store Full Chat History": StoreFullChatHistory,
