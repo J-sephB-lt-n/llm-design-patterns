fe65a4d7df609d8cbf2a878547c976b1caa49a8c
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2025-09-10 21:34:31 +0200

    feat(web_browser/examples/manual_tool_exploration.py): works amazingly!

--- tool_use/tools/web_browser/examples/manual_tool_exploration.py
+++ tool_use/tools/web_browser/examples/manual_tool_exploration.py
@@ -3,8 +3,12 @@ CLI interface for manually testing out the tools available to a web browsing age
 """
 
 import asyncio
+import inspect
+import sys
 from collections.abc import Callable
-from typing import Final
+from typing import Final, get_type_hints
+
+import questionary
 
 from tool_use.tools.web_browser import (
     BrowserManager,
@@ -19,6 +23,19 @@ AGENT_TOOLS: Final[dict[str, Callable]] = {
 }
 
 
+def list_func_args(func: Callable) -> list[tuple[str, type]]:
+    """List the arguments of `func` (and their types)."""
+    sig = inspect.signature(func)
+    hints = get_type_hints(func)
+    args: list[tuple[str, type]] = []
+    for name in sig.parameters:
+        args.append(
+            (name, hints[name]),
+        )
+
+    return args
+
+
 async def main():
     """Run the CLI loop."""
     browser_manager = BrowserManager()
@@ -27,10 +44,23 @@ async def main():
     try:
         browser = WebBrowser(browser_manager)
         async with browser.isolated_browser_session():
-            test = input("Please provide a URL: ")
-            await go_to_url(test)
-            print("going there")
-            await asyncio.sleep(10)
+            while True:
+                user_choice = await questionary.select(
+                    "Please select a tool:",
+                    choices=[*AGENT_TOOLS.keys(), "exit"],
+                ).ask_async()
+                if user_choice == "exit" or user_choice is None:
+                    sys.exit(0)
+                print("Please provide tool arguments:")
+                func_kwargs = {}
+                for arg_name, arg_type in list_func_args(AGENT_TOOLS[user_choice]):
+                    func_kwargs[arg_name] = arg_type(
+                        input(f"{arg_name} ({arg_type.__name__}): "),
+                    )
+                print(f"executing tool [{user_choice}]")
+                func_result = await AGENT_TOOLS[user_choice](**func_kwargs)
+                print(func_result)
+
     finally:
         await browser_manager.shutdown_browser()
 
